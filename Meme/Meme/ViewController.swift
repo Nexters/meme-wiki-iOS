23c8b49a16f1d64938b8e6268ed89ab7b3618c61
leemhyungyu <lhglbkksj@naver.com>
2025-07-15 22:31:45 +0900

    chore: xcode.proj 생성

---
+++ Meme/Meme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by 임현규 on 7/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
