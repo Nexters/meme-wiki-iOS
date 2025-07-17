a0900ccc43c805566c0aa0f295baa0374c656d61
임현규 <48830320+leemhyungyu@users.noreply.github.com>
2025-07-17 23:20:14 +0900

    [Settings/#1] xcode project setting (#2)
    * chore: xcode.proj 생성
    
    * chore: Main.storyboard 삭제
    
    - 관련 info.plist, Build Setting 수정
    
    * chore: release, dev scheme & configuration 설정
    
    * feat: scheme에 따른 xcconfig 설정
    
    - dev scheme -> Debug Configuration
    - release scheme -> Release Configuration
    - 각 scheme, configuration에 따른 확인을 테스트하기 위해 ViewController Label로 구분
    
    * chore: SPM 설정
    
    - Alamofire, Moya
    
    * chore: Kingfisher 추가
    
    * chore: .gitignore 재적용
    
    * chore: App Target build configuration 설정
    
    - Meme-release -> Release build Configuration
    - Meme-dev -> Development build Configuration
    
    * chore: Meme-dev target info.plist 경로 수정

---
+++ Meme/Meme/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by 임현규 on 7/15/25.
+//
+
+import UIKit
+import Alamofire
+import Moya
+import Kingfisher
+
+class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        let labelText = Bundle.main.object(forInfoDictionaryKey: "APP_LABEL_TEXT") as? String
+        label.text = labelText
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(label)
+        view.backgroundColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
