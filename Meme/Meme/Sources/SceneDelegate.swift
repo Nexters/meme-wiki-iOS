fe1eb04b8a36d2fe704666c853424fc3609210c0
dayo2n <0217dayun@naver.com>
2025-07-26 16:31:00 +0900

    feat: 웹뷰 메세지 처리 제대로 동작하도록 수정 (#5)

--- Meme/Meme/Sources/SceneDelegate.swift
+++ Meme/Meme/Sources/SceneDelegate.swift
@@ -10,12 +10,12 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
         self.window = window
         window.makeKeyAndVisible()
     }
