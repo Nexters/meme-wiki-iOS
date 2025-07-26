8064b34ee43b8c6fb3deeb26a77f6734abf724bf
dayo2n <0217dayun@naver.com>
2025-07-26 11:24:52 +0900

    feat: 웹뷰브릿지, 웹뷰컨트롤러 셋업 (#5)

---
+++ Meme/Meme/Sources/Base/Web/Bridge/WikiScriptMessageHandler.swift
@@ -0,0 +1,39 @@
+//
+//  WikiScriptMessageHandler.swift
+//  Meme
+//
+//  Created by 제나 on 7/26/25.
+//
+
+import WebKit
+
+class WikiScriptMessageHandler: NSObject, WKScriptMessageHandler {
+    private weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController? = nil) {
+        self.viewController = viewController
+    }
+    
+    func userContentController(
+        _ userContentController: WKUserContentController,
+        didReceive message: WKScriptMessage
+    ) {
+        guard let parameters = message.body as? [String: Any],
+              let command = parameters["command"] as? String, // TODO: - command?
+              let handlerName = WikiScriptMessageHandlerName(rawValue: command)
+        else { return }
+        
+        switch handlerName {
+        case .close:
+            return
+        case .share:
+            let method = parameters["method"] as? String
+            let link = (parameters["link"] as? String)?.asEncodedURL()
+        }
+    }
+}
+
+enum WikiScriptMessageHandlerName: String, CaseIterable {
+    case close
+    case share
+}
---
+++ Meme/Meme/Sources/Base/Web/WikiWebViewController.swift
@@ -0,0 +1,67 @@
+//
+//  WikiWebViewController.swift
+//  Meme-release
+//
+//  Created by 제나 on 7/26/25.
+//
+
+import WebKit
+
+class WikiWebViewController: UIViewController {
+    let url: URL
+    var webViews = [WKWebView]()
+    
+    init(url: URL) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setupWebView() {
+        let webView = createWebView(frame: view.frame, configuration: configuration)
+        load(url, in: webView)
+    }
+    private func load(_ url: URL, in webView: WKWebView) {
+        webView.load(URLRequest(url: url))
+    }
+}
+
+extension WikiWebViewController: WKUIDelegate, WKNavigationDelegate {
+    private func createWebView(frame: CGRect, configuration: WKWebViewConfiguration) -> WKWebView {
+        let webView = WKWebView(frame: frame, configuration: configuration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        webViews.append(webView)
+        return webView
+    }
+    private var configuration: WKWebViewConfiguration {
+        var configuration = WKWebViewConfiguration()
+        configuration.allowsAirPlayForMediaPlayback = true
+        return configuration
+    }
+    private var userContentController: WKUserContentController {
+        let contentController = WKUserContentController()
+        contentController.add(wikiScriptMessageHandler, contentWorld: .page, name: "MemeWiki")
+        return contentController
+    }
+    private var wikiScriptMessageHandler: WKScriptMessageHandler {
+        return WikiScriptMessageHandler()
+    }
+}
