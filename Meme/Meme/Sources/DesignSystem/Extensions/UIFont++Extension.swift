0938cd7cb9da94009a4dbd54d46a976fd1a02c0e
leemhyungyu <lhglbkksj@naver.com>
2025-07-27 21:48:36 +0900

    feat: Custom Font 적용 메서드 구현
    - NSAttributedString을 통해 자간, 행간 설정

---
+++ Meme/Meme/Sources/DesignSystem/Extensions/UIFont++Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont++Extension.swift
+//  Meme
+//
+//  Created by 임현규 on 7/27/25.
+//
+
+import UIKit
+
+extension UIFont {
+    static func customFont(_ font: CustomFont) -> UIFont {
+        return UIFont(name: font.name, size: font.size)!
+    }
+}
+
