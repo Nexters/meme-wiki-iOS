c3844e051db0ab4731ae450783861cd1f71a69a2
leemhyungyu <lhglbkksj@naver.com>
2025-08-01 21:14:14 +0900

    feat: Galmuri body 추가

--- Meme/Meme/Sources/DesignSystem/Font/Galmuri.swift
+++ Meme/Meme/Sources/DesignSystem/Font/Galmuri.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+enum Galmuri: CustomFontConvertible {
+    case body(Body)
+    
+    enum Body {
+        case body1
+    }
+    
+    var name: String {
+        switch self {
+        case .body: return "Galmuri7"
+        }
+    }
+    
+    var size: CGFloat {
+        switch self {
+        case .body(let level):
+            switch level {
+            case .body1: return 14
+            }
+        }
+    }
+    
+    var letterSpacing: CGFloat { return -0.6 }
+    
+    var lineHeight: CGFloat {
+        switch self {
+        case .body(let level):
+            switch level {
+            case .body1: return 18
+            }
+        }
+    }
+}
