769cef1815f3f1cb4a5d574cd8a5389d05f11788
leemhyungyu <lhglbkksj@naver.com>
2025-08-03 23:42:51 +0900

    feat: search section, item 구현
    - UICollectionViewDiffableDataSource을 사용하기 위한 ..

--- Meme/Meme/Sources/Wiki/Domain/Entity/Thumbnail.swift
+++ Meme/Meme/Sources/Wiki/Domain/Entity/Thumbnail.swift
@@ -5,7 +5,7 @@
 //  Created by 임현규 on 7/29/25.
 //
 
-struct Thumbnail {
+struct Thumbnail: Hashable {
     var year: Int
     var title: String
     var hashtag: [String]
---
+++ Meme/Meme/Sources/Wiki/Presentation/Search/Model/MemeSearchSection.swift
@@ -0,0 +1,72 @@
+//
+//  MemeSearchSection.swift
+//  Meme
+//
+//  Created by 임현규 on 8/3/25.
+//
+
+import Foundation
+import UIKit
+
+enum MemeSearchSection: Hashable {
+    case meme
+}
+
+enum MemeSearchDisplayItem: Hashable {
+    case list(MemeSearchItem)
+    case grid(MemeSearchItem)
+    case empty
+}
+
+extension MemeSearchDisplayItem {
+    var itemSize: NSCollectionLayoutSize {
+        switch self {
+        case .grid:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.5),
+                heightDimension: .fractionalWidth(0.5)
+            )
+        case .list:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(100)
+            )
+        case .empty:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        }
+    }
+
+    var groupSize: NSCollectionLayoutSize {
+        switch self {
+        case .grid:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(0.5)
+            )
+        case .list:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(100)
+            )
+        case .empty:
+            return NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        }
+    }
+
+    var itemInset: NSDirectionalEdgeInsets {
+        switch self {
+        case .grid:
+            return .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+        case .list:
+            return .init(top: 6, leading: 20, bottom: 6, trailing: 20)
+        case .empty:
+            return .zero
+        }
+    }
+}
