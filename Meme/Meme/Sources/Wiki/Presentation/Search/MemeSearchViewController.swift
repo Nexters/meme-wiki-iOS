1e49adcae836f51aba3bd8ff0ec3bfbd733cfbfb
leemhyungyu <lhglbkksj@naver.com>
2025-08-04 12:50:56 +0900

    feat: DiffableDataSource 설정

--- Meme/Meme/Sources/Wiki/Presentation/Search/MemeSearchViewController.swift
+++ Meme/Meme/Sources/Wiki/Presentation/Search/MemeSearchViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import Combine
 
 final class MemeSearchViewController: BaseViewController {
+    typealias SearchDataSource = UICollectionViewDiffableDataSource<MemeSearchSection, MemeSearchDisplayItem>
+    private var dataSource: SearchDataSource?
     
     private lazy var searchTextField: SearchTextField = {
         let textField = SearchTextField()
@@ -16,9 +18,15 @@ final class MemeSearchViewController: BaseViewController {
         textField.layer.cornerRadius = Constants.SearchTextField.cornerRadius
         return textField
     }()
+    
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }()
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDataSource()
     }
     
     override func configureUI() {
@@ -30,6 +38,24 @@ final class MemeSearchViewController: BaseViewController {
             searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.SearchTextField.trailing),
             searchTextField.heightAnchor.constraint(equalToConstant: Constants.SearchTextField.height)
         ])
+    }
+    
+    func configureDataSource() {
+        dataSource = SearchDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            switch itemIdentifier {
+            case .list(let item):
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemeSearchListCell.identifier, for: indexPath) as? MemeSearchListCell else { return .none }
+                cell.updateUI(item)
+                return cell
+            case .grid(let item):
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemeSearchGridCell.identifier, for: indexPath) as? MemeSearchGridCell else { return .none }
+                cell.updateUI(item.thumbnail)
+                return cell
+            case .empty:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemeSearchEmptyCell.identifier, for: indexPath) as? MemeSearchEmptyCell else { return .none }
+                return cell
+            }
+        })
     }
 }
 
