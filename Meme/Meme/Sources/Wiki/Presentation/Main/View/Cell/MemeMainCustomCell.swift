89fb03a698803546159867c1168d1e4a7e0df890
dayo2n <0217dayun@naver.com>
2025-08-05 22:02:40 +0900

    feat: 메인 - 상단 커스텀셀 구현 (#18)

--- Meme/Meme/Sources/Wiki/Presentation/Main/View/Cell/MemeMainCustomCell.swift
+++ Meme/Meme/Sources/Wiki/Presentation/Main/View/Cell/MemeMainCustomCell.swift
@@ -5,4 +5,32 @@
 //  Created by 제나 on 8/5/25.
 //
 
-import Foundation
+import UIKit
+
+final class MemeMainCustomCell: UICollectionViewCell {
+    static let identifier = "MemeMainCustomCell"
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        for sub in contentView.subviews {
+            sub.removeFromSuperview()
+        }
+    }
+    
+    func configureCell() {
+        contentView.backgroundColor = .systemGray5
+        contentView.layer.cornerRadius = 12
+        contentView.layer.masksToBounds = true
+        
+        let imageview = UIImageView()
+        imageview.image = UIImage(resource: .bannerCustomImage1)
+        imageview.contentMode = .scaleAspectFill
+        contentView.addSubview(imageview)
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageview.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageview.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageview.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageview.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)])
+    }
+}
