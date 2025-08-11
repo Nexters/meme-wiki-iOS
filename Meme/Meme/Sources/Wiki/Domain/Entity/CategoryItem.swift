238ca537c0d3b71ccef9345861c0d7f3f386a0a9
leemhyungyu <lhglbkksj@naver.com>
2025-08-11 21:59:47 +0900

    feat: CategoryItem 구현

---
+++ Meme/Meme/Sources/Wiki/Domain/Entity/CategoryItem.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryItem.swift
+//  Meme
+//
+//  Created by 임현규 on 8/11/25.
+//
+
+import Foundation
+
+struct CategoryItem: Hashable {
+    var id: Int
+    var title: String
+    var imageURL: String
+}
