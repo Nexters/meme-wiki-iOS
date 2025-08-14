cefb3499a3b146517b90efab46bee2fe6fa784f2
dayo2n <0217dayun@naver.com>
2025-08-15 00:05:41 +0900

    feat: 메인 - 인급밈 그라데이션 적용 및 리팩토링 (#36)

--- Meme/Meme/Sources/DesignSystem/Extensions/UIView++Extension.swift
+++ Meme/Meme/Sources/DesignSystem/Extensions/UIView++Extension.swift
@@ -53,4 +53,19 @@ extension CAGradientLayer {
         ]
         self.locations = [0.0, 0.65, 1.0]
     }
+    
+    func makeDimmedLayer() -> CAGradientLayer {
+        let gradient = CAGradientLayer()
+        
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        gradient.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.2).cgColor,
+            UIColor.black.cgColor
+        ]
+        gradient.locations = [0.0, 0.65, 1.0]
+        
+        return gradient
+    }
 }
